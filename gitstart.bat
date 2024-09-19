976042412e65e0f944d76569e8201dc25119578d
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-19 15:01:54 +0200

    updated .gitignore

--- gitstart.bat
+++ gitstart.bat
@@ -8,6 +8,7 @@ REM Step 2: Add a .gitignore file (you can modify this part to add specific igno
 echo # Ignore temporary files > .gitignore
 echo dummy.py >> .gitignore
 echo /__pycache__ >> .gitignore
+echo gitstart.bat >> .gitignore
 
 REM Step 3: Add all files to the repository
 git add .
@@ -17,6 +18,9 @@ git commit -m "Initial commit"
 
 REM Step 5: Add the remote repository (replace with your actual remote URL)
 git remote add origin https://github.com/GioseaxMC/MyForexNews
+
+git pull origin main --allow-unrelated-histories
+
 
 REM Step 6: Push the changes to the remote repository (main branch)
 git push -u origin main
